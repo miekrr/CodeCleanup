72eba091b40d0b01a0285637082eeb435b4340f1
s0th77 <mihai_deviant@yahoo.com>
2015-08-08 16:05:36 +0300

    Initial commit. Added test data.

---
+++ test-data/source.c
@@ -0,0 +1,43 @@
+
+if((something == -1) && (somethingelse == -1) && (etc == -1)) {
+    // ... 1
+}
+
+if((something == -1) && (somethingelse == -1) ) {
+    // ... 2
+} 
+
+if((something == -1) && 
+	((somethingelse == -1) &&
+	 (etc == -1)) {
+    // ... 3
+}
+
+if((something == -1) && 
+	((somethingelse == -1) &&  (test == 1) &&
+	 (etc == -1)) {
+    // ... 4
+}
+
+
+if( (something == -1) && 
+	somethingelse == -1 && 
+	(etc == -1)) {
+    // ... 5
+}
+
+case juju:
+ // yeye
+break;
+
+case somethingelse:
+	i++;
+break;
+
+case jajajjaja:
+
+case uncase:
+case somethingelse:
+case juju:
+	i++;
+break;

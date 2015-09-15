7df2977b0c3fd15afe8f208a70dd704c5f5fae25
miekrr <suprarealistul@gmail.com>
2015-09-16 02:09:23 +0300

    Rewritten all the enum matching and removal.

--- verify.py
+++ verify.py
@@ -4,99 +4,138 @@ import sys
 import re
 import string
 
-REGEX_CASE_LINE_MATCH = '[ \t]*case[ \t][\d\w]*:'
-TEST_FILE = 'test-data/source.c'
 
-def getRegexFor(varName):
-    return  '[ \t]*' + varName + '[ =!]*[-][\d\w]'
+def isNumeric(char) :
+    return char.isnumeric()
 
 
-def nextCharIsParentesis(line):
-    for i in range(1, len(line)):
-        if line[i] == " " :
-            continue
+def is_next_char_word(line, idx):   
+    if line[idx].isalpha() or line[idx] == "_" or isNumeric(line[idx]) :
+        return True
+    return False
+
+
+def is_next_char_whitespace(line, idx):
+    if len(line) < idx :
+        if line[idx] == "_" or line[idx] == "\t":
+            return True
+    return False
+
+def variable_matched(line, varToBeRemoved):
+    if varToBeRemoved in line:
+        idx = line.index(varToBeRemoved)
+        end_idx = idx + len(varToBeRemoved)
+        if idx == -1:
+            return False, None, None
         else :
-            if line[i] == ")" :
-                return i
+            return True, idx, end_idx
+    else :
+        return False, None, None
+
+
+def is_variable_in_paranthesis(line, end_var) :
+    remove_statement = False
+    start_statement = None
+    while is_next_char_whitespace(line, end_var):
+        end_var = end_var + 1
+    if len(line) <= end_var :
+        return False, None, None
+    current_idx = end_var
+    if line[current_idx] == ")" :
+        end_var = current_idx + 1 
+        remove_statement = True
+
+    if remove_statement == True :
+        balance = 1
+        current_idx = end_var - 1
+        while balance != 0 :
+            current_idx = current_idx - 1
+            if line[current_idx] == ")" :
+                balance = balance + 1;
+            if line[current_idx] == "(" :
+                balance = balance - 1;
+        start_statement = current_idx
+    else :
+        return remove_statement, None, None
+    before = line[start_statement-2: start_statement]
+
+    while line[end_var] == " " :
+        end_var = end_var + 1
+
+    after = line[end_var:end_var+2]
+
+    if after == "&&" or after == "||" :
+        end_var = end_var + 2
+    else :
+        if before == "&&" or before == "||" :
+            start_statement = start_statement - 2
+
+    return remove_statement, start_statement, end_var
+
+
+def is_char_part_of_statement(char):
+    if char.isalpha() or char == "_" or isNumeric(char) or char == " " :
+        return True
+    return False
+
+
+def get_start_of_statement(line, start_var):
+    while is_char_part_of_statement(line[start_var]) == True:
+        start_var = start_var - 1
+
+    operator = line[start_var - 1: start_var + 1]
+    if operator == "==" or operator == "!=" :
+        start_var = start_var - 1
+        while is_char_part_of_statement(line[start_var-1]) == True:
+            start_var = start_var - 1
+    
+    return start_var
+
+
+def get_end_of_statement(line, end_var):
+    if len(line) > end_var :
+        while line[end_var] == " " :
+            end_var = end_var + 1
+        after = line[end_var:end_var + 2]
+        if after == "&&" or after == "||" :
+            end_var = end_var + 2
+    return end_var
+
+
+def processFile(cData, varToBeRemoved):
+    changed_file = []
+
+    for line in cData:
+        skipline = False
+        var_matched, start_var, end_var = variable_matched(line, varToBeRemoved)
+        if var_matched == True :
+            recalculate_start_end, start, end = is_variable_in_paranthesis(line, end_var)    
+
+            if recalculate_start_end == True :
+                start_statement = start
+                end_statement = end
             else :
-                break
-    return -1
+                start_statement = get_start_of_statement(line, start_var)
+                end_statement = get_end_of_statement(line, end_var)
 
-
-def prevCharIsParentesis(line, pos):
-    l = len(line[0:pos])
-
-    for i in reversed(range(1, l)):
-        if line[i] == " " :
-            continue
-        else :
-            if line[i] == "(" :
-                return i
-            else :
-                break
-    return -1
-
-
-def getNextDataIdx(line):
-    empty_list = [ " ", "\t", "\n", "&", "|"] 
-    for i in range(1, len(line)) :
-        if line[i] in empty_list :
-            continue
-        else :
-            return i;
-
-    return -1
-
-
-def isParenthesisEmpty(line, pos):
-    pos = pos - 1
-    start_pos = line.find("(", pos)
-    end_pos = line.find(")", pos) + 1
-    if start_pos is not -1:
-        line = line[0:start_pos]+line[end_pos:]
-
-    pos = pos - 1
-    start_pos = line.find("&&", pos) - 2
-    end_pos = line.find("&&", pos)
-    infile = line[start_pos:end_pos]
-    if infile.strip() == "":
-        line = line[0:start_pos-2] + line[end_pos:]
-
-    if nextCharIsParentesis(line[pos:]) != -1 :
-        line = line[0:pos-2] + line[nextCharIsParentesis(line[pos:]) + pos]
-        return line
-
-    if prevCharIsParentesis(line, pos+1) != -1 :
-        if getNextDataIdx(line[:pos+1]) != -1 :
-            idx = getNextDataIdx(line[:pos+1]) 
-            line = line[0:pos + 1] + line[getNextDataIdx(line[pos:]):]
-            line = line.rstrip()
-
-        return line
-
-    return line
-
-
-def processFile(fileName, varToBeRemoved):
-    cfile = open(fileName, 'r+')
-    for line in cfile:
-        m = re.search(getRegexFor(varToBeRemoved), line)
-        line = re.sub(getRegexFor(varToBeRemoved), '', line)
-
-        if m is not None:
-            line = isParenthesisEmpty(line, m.start())
+            line = line[:start_statement] + line[end_statement:]
             if "&&" == line.strip() or  "||" == line.strip():
-                line = ""
+                skipline = True
+            else :
+                tmp_line = line[start_statement-1:].strip()
+                if "&&" == tmp_line or "||" == tmp_line :
+                    line = line[:start_statement-1]
+            line.rstrip()
 
-        print(line, end="")
+        if skipline == False :
+            changed_file.append(line.rstrip("\n"))
+    return changed_file
 
 
-def removeCase(fileName, enumIdx) :
-    with open(fileName, "r") as cFile:
-        cData = cFile.readlines()
-
+def removeCase(cData, enumIdx) :
     skipLine = False
     fCase = 'case ' + enumIdx + ':'
+    changed_file = []
     for idx in range(len(cData)):
         if cData[idx].find(fCase) != -1:
             skipLine = True
@@ -106,16 +145,13 @@ def removeCase(fileName, enumIdx) :
             skipLine = False
             continue
 
+        if cData[idx].find("default") != -1 and skipLine == True:
+            skipLine = False
+
         if re.search(REGEX_CASE_LINE_MATCH, cData[idx]) is not None :
             skipLine = False
 
         if not skipLine:
-            print(cData[idx].rstrip())
+            changed_file.append(cData[idx].rstrip("\n"))
+    return changed_file
 
-
-
-"""  !!! actual processing !!! """
-#processFile(TEST_FILE, 'somethingelse')
-#removeCase(TEST_FILE, 'somethingelse')
-
-print("")
